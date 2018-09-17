153eaf674962d2ee4826eb0812e91eb6e8a6511c
Terrin Day <terrinday@gmail.com>
2018-09-17 04:32:52 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,12 +2,20 @@ var playlist = new Object ({ artistName: 'songTitle' });
 
 function updatePlaylist(playlist, artistName, songTitle)
 {
+<<<<<<< HEAD
 playlist[artistName] = songTitle;
   return playlist;
+=======
+return Object.assign({}, playlist, {[artistName]: songTitle })
+>>>>>>> 2fb0a14802ae88b30444c849eb0620eb8f52b443
 }
 
 function removeFromPlaylist(playlist, artistName)
 {
+<<<<<<< HEAD
   delete playlist[artistName]
+=======
+  delete playlist.artistName;
+>>>>>>> 2fb0a14802ae88b30444c849eb0620eb8f52b443
   return playlist
 }
